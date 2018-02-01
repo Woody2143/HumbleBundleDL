194c23109bd7a92c4712d9e8169af244154decc8
Brandon Wood <Woody@2143.net>
2018-02-01 18:20:54 -0500

    basic working script
    basic working script

--- HumbleBundle.pl
+++ HumbleBundle.pl
@@ -42,8 +42,13 @@ if ( $allOrdersResponse->is_success  ) {
     $ordersList = parse_json $allOrdersResponse->decoded_content;
 }
 
+my %items;
+
+print 'Getting Bundles ';
+
 for my $order ( @{$ordersList} ) {
-    say $order->{gamekey};
+
+    print '.';
 
     my $getOrderURL = 'https://www.humblebundle.com/api/v1/order/' . $order->{gamekey};
     my $orderResponse = $ua->get($getOrderURL, Cookie => $cookie );
@@ -52,56 +57,94 @@ for my $order ( @{$ordersList} ) {
     if ( $orderResponse->is_success  ) {
         $orderDetails = parse_json $orderResponse->decoded_content;
     }
-    say $orderDetails->{product}->{human_name};
+
+    ( my $dirName = $orderDetails->{product}->{human_name} ) =~ s/\s+/_/g;
+
+    $dirName =~ s/(:|,|_\|\|)/_-/g;
+    $dirName =~ s/('|!)//g;
+    $dirName =~ s/&/and/g;
+    $items{$order->{gamekey}} = {
+        name    => $orderDetails->{product}->{human_name},
+        created => $orderDetails->{created},
+        dir     => $dirName,
+        books   => $orderDetails->{subproducts}
+    };
+
 }
-die 'The End';
+print "\n";
+say 'Sorting Bundles';
+foreach my $key (sort { $items{$a}->{created} cmp $items{$b}->{created} } (keys %items) ) {
+    say $key . ' - ' . $items{$key}->{created} . ' - ' .  $items{$key}->{name};
+}
+my @keys;
+my $question = 0;
+while ($question == 0) {
+    print "Which key? ";
+    chomp (my $key = <STDIN>);
+    if ($key ne "") {
+        push @keys, $key;
+    } else {
+        $question = 1;
+    }
+}
 
-my $data;
-for my $book ( @{$data->{subproducts}} ) {
+for my $key (@keys) {
 
-    if ( @{$book->{downloads}} ) {
-        ( my $filename = $book->{human_name} ) =~ s/\s+/_/g;
-
-        $filename =~ s/(:|,|_\|\|)/_-/g;
-        $filename =~ s/('|!)//g;
-        $filename =~ s/&/and/g;
-        say "  Working on $filename -";
-
-        for my $file ( @{$book->{downloads}->[0]->{download_struct}} ) {
-
-            my $ext = lc $file->{name};
-            say "    Working on $ext -";
-
-            my $url = $file->{url}->{web};
-
-            say   "      downloading";
-            $ua->show_progress(1);
-            my $fileResponse = $ua->get($url);
-            die $fileResponse->status_line if !$fileResponse->is_success;
-
-            my $downloadedFile = $fileResponse->decoded_content( charset => 'none' );
-            my $md5HEX = md5_hex($downloadedFile);
-
-            if ( $md5HEX eq $file->{md5} ) {
-                say "      md5 values match";
-
-                my $fullFilename = $filename . '.' . $ext;
-                my $save = "/home/bwood/Dropbox/Books/Hacking_Reloaded_-_Humble_Bundle/$fullFilename";
-
-                unless(open SAVE, '>'.$save) {
-                    die "      Cannot create save file '$save'\n";
-                }
-                say "      Saving file: $save\n";
-                print SAVE $downloadedFile;
-                close SAVE;
-            } else {
-                say "      md5 values does not match!";
-                say "        " . $file->{md5} . " - md5 from JSON";
-                say "        $md5HEX - downloaded File";
-            }
-        }
-    die;
-
+    my $dir = '/home/bwood/Dropbox/Books/' . $items{$key}{dir};
+    if (-e $dir && -d $dir) {
+        say "$dir exists";
+    } else {
+        mkdir $dir or die "ERROR: Cannot create $dir!";
+        say "$dir created!";
     }
 
+    say 'Working on bundle: ' . $items{$key}{name};
+
+    for my $book ( @{$items{$key}{books}} ) {
+
+        if ( @{$book->{downloads}} ) {
+            ( my $filename = $book->{human_name} ) =~ s/\s+/_/g;
+
+            $filename =~ s/(:|,|_\|\|)/_-/g;
+            $filename =~ s/('|!)//g;
+            $filename =~ s/&/and/g;
+            say "  Working on $filename -";
+
+            for my $file ( @{$book->{downloads}->[0]->{download_struct}} ) {
+
+                my $ext = lc $file->{name};
+                say "    Working on $ext -";
+
+                my $url = $file->{url}->{web};
+
+                say   "      downloading";
+                $ua->show_progress(1);
+                my $fileResponse = $ua->get($url);
+                die $fileResponse->status_line if !$fileResponse->is_success;
+
+                my $downloadedFile = $fileResponse->decoded_content( charset => 'none' );
+                my $md5HEX = md5_hex($downloadedFile);
+
+                if ( $md5HEX eq $file->{md5} ) {
+                    say "      md5 values match";
+
+                    my $fullFilename = $filename . '.' . $ext;
+                    my $save = "$dir/$fullFilename";
+
+                    unless(open SAVE, '>'.$save) {
+                        die "      Cannot create save file '$save'\n";
+                    }
+                    say "      Saving file: $fullFilename\n";
+                    print SAVE $downloadedFile;
+                    close SAVE;
+                } else {
+                    say "      md5 values does not match!";
+                    say "        " . $file->{md5} . " - md5 from JSON";
+                    say "        $md5HEX - downloaded File";
+                }
+            }
+
+        }
+
+    }
 }
